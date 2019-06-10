5f325d813738763661ed44274c252b3d27a6b57f
Carlos Brown <carlos.brown3@gmail.com>
2019-06-10 18:27:41 -0400

    Fix draw_bs_reps

--- datascience.py
+++ datascience.py
@@ -57,7 +57,7 @@ def draw_bs_reps(data, func, size=1):
         """Draw bootstrap replicates."""
 
         # Initialize array of replicates: bs_replicates
-        bs_replicates = np.empty(size=size)
+        bs_replicates = np.empty(shape=size)
 
         # Generate replicates
         for i in range(size):
